d4f2b928969777e0714e5c7c77913204eb56e0fe
virtualjames <haveumetjames@gmail.com>
2015-06-06 02:47:08 -0700

    now it displays a rectangle

--- splash2d/main.cpp
+++ splash2d/main.cpp
@@ -1,11 +1,18 @@
-#include "mainwindow.h"
 #include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsView>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
 
+    // Create a scene
+    QGraphicsScene * mygamescene = new QGraphicsScene();
+    QGraphicsRectItem * myrect = new QGraphicsRectItem();
+    myrect->setRect(50,50,50,50);
+    mygamescene->addItem(myrect);
+    QGraphicsView * myview = new QGraphicsView(mygamescene);
+    myview->show();
     return a.exec();
 }
